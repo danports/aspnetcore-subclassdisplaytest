2963402d77b444d3cad104cd73943b66e0af4017
Dan Ports <dan@codewithin.com>
2020-09-16 09:54:28 -0400

    Initial import.

---
+++ SubclassDisplayTest/Models/DetailsModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SubclassDisplayTest.Models
+{
+    public class Widget
+    {
+        public int A { get; set; } = 3;
+    }
+
+    public class Subwidget : Widget
+    {
+        public int B { get; set; } = 6;
+    }
+
+    public class DetailsModel
+    {
+        public Widget Widget { get; set; }
+    }
+}
